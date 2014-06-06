2c0f8c847de5c9dc573f64f60c6ef08c289e67b8
Jade <jade.applegate@gmail.com>
2014-06-05 21:02:29 -0700

    json call almost working

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,2 +1,34 @@
 class LibrariesController < ApplicationController
+ before_action :set_library, only: [:show, :update, :destroy]
+  respond_to :json
+
+def index
+  respond_with Library.all
+end
+
+def create
+  respond_with Library.create(library_params)
+end
+
+def show
+respond_with @library
+end
+
+def update
+  respond_with @library.update(library_params)
+end
+
+def destroy
+  respond_with @library.destroy
+end
+
+private
+  def set_library
+    @library = Library.find(params[:id])
+  end
+
+  def library_params
+    params.require(:library).permit(:title, :owner)
+  end
+
 end
